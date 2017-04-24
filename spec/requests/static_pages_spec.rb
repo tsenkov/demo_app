4bf57325ada3b9733a446d5395ccd3978a87c25d
Tsvetko Tsenkov <tsenkov@users.noreply.github.com>
2017-04-24 16:02:51 -0400

    s

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "Home page", type: :request do
-  it "should have the content 'Sample App'" do
-    visit '/static pages/home'
-    page.should have content('Sample App')
+
+describe "StaticPages" do
+  describe "Home Page" do
+    it "works! (now write some real specs)" do
+      get '/static_pages/home'
+      expect(response).to have_http_status(200)
+
+    end
+
+    describe "Help Page" do
+      it "Should have content help" do
+
+        get '/static_pages/help'
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "About Page" do
+      it "Should have content about" do
+        get '/static_pages/about'
+        expect(response).to have_http_status(200)
+                
+      end
+    end
   end
 end
