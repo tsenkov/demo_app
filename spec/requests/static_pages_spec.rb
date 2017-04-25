8b81e3f0a6ce07b8a959cb72d34a2841fd9a873a
Tsvetko Tsenkov <tsenkov@users.noreply.github.com>
2017-04-24 22:13:32 -0400

    added rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 
-describe "StaticPages" do
+RSpec.describe "StaticPages" do
+  let(:base_title) {'Sample App Title'}
+
   describe "Home Page" do
-    it "works! (now write some real specs)" do
+    it "Check title having base and not having |" do
       get '/static_pages/home'
       expect(response).to have_http_status(200)
 
+      visit '/static_pages/home'
+
+      expect(page).to have_title "#{base_title}"
+      expect(page).not_to have_title "|"
     end
 
     describe "Help Page" do
@@ -14,6 +20,9 @@ describe "StaticPages" do
 
         get '/static_pages/help'
         expect(response).to have_http_status(200)
+        visit '/static_pages/help'
+        expect(page).to have_title "#{base_title}"
+        expect(page).to have_title "| Help"
       end
     end
 
@@ -21,7 +30,9 @@ describe "StaticPages" do
       it "Should have content about" do
         get '/static_pages/about'
         expect(response).to have_http_status(200)
-                
+        visit '/static_pages/about'
+        expect(page).to have_title "#{base_title}"
+        expect(page).to have_title "| About"
       end
     end
   end
